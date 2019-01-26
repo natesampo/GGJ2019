e4b98667e6d4af392c985110459af26bb22f4d7d
Daniel Alhadeff <dalhadeff@olin.edu>
2019-01-26 01:26:49 -0500

    Tile Work

--- src/game/Tile.java
+++ src/game/Tile.java
@@ -4,13 +4,19 @@ import game.GameObject.Animations;
 
 public class Tile extends GameObject {
 
-	public Tile(int x, int y) {
+	private int type;
+	public Tile(int x, int y, int type) {
 		super(x, y);
+		this.type = type;
 	}
 
 	@Override
 	public void update(Game game, double dt) {
+		switch(type){
+		case 1:
 		this.sprite.animate(Animations.WATER, dt);
+		break;
+	}	
 	}
 
 	@Override
